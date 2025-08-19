31c3ba12ac63a2519bd2100240cc9c894e598942
Jan David <jdno@jdno.dev>
2025-08-19 11:05:46 +0200

    Implement a color generator for Tailwind CSS
    The first color generator of Labelflair simply takes the color palettes
    of [Tailwind CSS] to generate its colors. The colors have been hand
    picked and look good, and the generator's logic is simply looking up
    colors in hardcoded lists.
    
    [tailwind css]: https://tailwindcss.com/docs/colors

--- crates/labelflair/src/lib.rs
+++ crates/labelflair/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! Generate a colorful palette of labels for your GitHub Issues
+//!
+//! Labelflair is a library and command-line application that generates colorful labels for GitHub
+//! Issues. It accepts a list of labels, generates a palette of colors for them, and outputs them in
+//! a format that can be used to create or update labels in a GitHub repository.
+//!
+//! The library of Labelflair does the heavy lifting of generating the colors and can be used in
+//! other applications or scripts. The command-line application provides a simple interface to use
+//! the library and generate labels directly from a configuration file.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// We are enforcing documentation on all public items in this crate to ensure that the library is
+// well-documented and easy to use. Documentation on private items is not required but encouraged.
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+use typed_fields::name;
+
+pub mod colors;
+
+name!(
+    /// A color in Labelflair
+    Color
+);
