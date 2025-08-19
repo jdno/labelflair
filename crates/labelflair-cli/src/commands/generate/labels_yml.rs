ff02faa7668c34c0d2dd11a0fd9e0c1b897b3526
Jan David <jdno@jdno.dev>
2025-08-19 22:52:50 +0200

    Remove dead code

--- crates/labelflair-cli/src/commands/generate/labels_yml.rs
+++
@@ -1,8 +0,0 @@
-use clap::Args;
-use clawless::command;
-
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, Args)]
-struct LabelsYmlArgs {}
-
-#[command]
-async fn labels(args: LabelsYmlArgs) {}
