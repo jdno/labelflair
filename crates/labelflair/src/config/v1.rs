1e4d65c0095c8e87e88338ba1bab817be2637fbf
Jan David <jdno@jdno.dev>
2025-08-20 21:39:47 +0200

    Support optional descriptions for labels
    Labels can now have optional descriptions. When a description is
    provided, it will be included in the generated output. To maintain the
    simplicity of the configuration when no descriptions are used, it is
    still possible to define labels just as a string.

--- crates/labelflair/src/config/v1.rs
+++ crates/labelflair/src/config/v1.rs
@@ -4,23 +4,15 @@
 
 use std::collections::HashMap;
 
-use getset::{CopyGetters, Getters};
+use getset::Getters;
 use serde::Deserialize;
 use typed_builder::TypedBuilder;
-use typed_fields::name;
 
-use crate::colors::{Colors, Generate};
-use crate::label::{Label, LabelName};
+pub use self::group::*;
+pub use self::label_variant::*;
 
-name!(
-    /// A name for a group of labels in Labelflair
-    GroupName
-);
-
-name!(
-    /// A prefix for labels in Labelflair
-    Prefix
-);
+mod group;
+mod label_variant;
 
 /// Configuration for Labelflair version 1
 ///
@@ -35,113 +27,13 @@ pub struct ConfigV1 {
     groups: HashMap<GroupName, Group>,
 }
 
-/// A group of labels in Labelflair
-///
-/// This struct represents a group of labels in Labelflair. Each group has an optional prefix, a
-/// color generator, and a list of labels. If a prefix is provided, it will be prepended to each
-/// label in the group.
-#[derive(
-    Clone,
-    Eq,
-    PartialEq,
-    Ord,
-    PartialOrd,
-    Hash,
-    Debug,
-    CopyGetters,
-    Getters,
-    Deserialize,
-    TypedBuilder,
-)]
-pub struct Group {
-    /// An optional prefix for the labels in this group
-    #[builder(setter(into))]
-    #[getset(get = "pub")]
-    #[serde(default)]
-    prefix: Option<Prefix>,
-
-    /// The color generator for this group
-    #[getset(get_copy = "pub")]
-    colors: Colors,
-
-    /// A list of labels in this group
-    #[getset(get = "pub")]
-    labels: Vec<LabelName>,
-}
-
-impl Group {
-    /// Expand the group into a list of labels
-    ///
-    /// This method generates a list of GitHub Issues labels, each with a name and a color. It does
-    /// this by generating a list of colors, optionally prefixing each label name, and then mapping
-    /// names and colors into [`Label`] instances.
-    pub fn expand(&self) -> Vec<Label> {
-        let colors = self.colors.generate(self.labels.len());
-        let prefix = self.prefix.clone().unwrap_or("".into());
-
-        // Sort labels to ensure a nice color palette in GitHub's user interface
-        let mut labels = self.labels.clone();
-        labels.sort();
-
-        labels
-            .iter()
-            .enumerate()
-            .map(|(i, label)| {
-                Label::builder()
-                    .name(format!("{prefix}{label}"))
-                    .color(colors[i].clone())
-                    .build()
-            })
-            .collect()
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use indoc::indoc;
 
-    use crate::colors::Tailwind;
+    use crate::colors::{Colors, Tailwind};
 
     use super::*;
-
-    #[test]
-    fn expand() {
-        let group = Group::builder()
-            .prefix(Prefix::new("C-"))
-            .colors(Colors::Tailwind(Tailwind::Red))
-            .labels(vec![LabelName::new("bug"), LabelName::new("feature")])
-            .build();
-
-        let labels = group.expand();
-        let expected = vec![
-            Label::builder().name("C-bug").color("#fca5a5").build(),
-            Label::builder().name("C-feature").color("#b91c1c").build(),
-        ];
-
-        assert_eq!(labels, expected);
-    }
-
-    #[test]
-    fn expand_sorts_labels() {
-        let group = Group::builder()
-            .prefix(Prefix::new("C-"))
-            .colors(Colors::Tailwind(Tailwind::Red))
-            .labels(vec![
-                LabelName::new("feature"),
-                LabelName::new("alpha"),
-                LabelName::new("bug"),
-            ])
-            .build();
-
-        let labels = group.expand();
-        let expected = vec![
-            Label::builder().name("C-alpha").color("#fecaca").build(),
-            Label::builder().name("C-bug").color("#ef4444").build(),
-            Label::builder().name("C-feature").color("#991b1b").build(),
-        ];
-
-        assert_eq!(labels, expected);
-    }
 
     #[test]
     fn trait_deserialize() {
@@ -149,7 +41,7 @@ mod tests {
             [categories]
             prefix = "C-"
             colors = { tailwind = "red" }
-            labels = ["bug", "feature"]
+            labels = ["bug", { name = "feature", description = "A new feature" }]
         "#};
 
         let config: ConfigV1 = toml::from_str(toml).unwrap();
@@ -159,7 +51,13 @@ mod tests {
                 Group::builder()
                     .prefix(Prefix::new("C-"))
                     .colors(Colors::Tailwind(Tailwind::Red))
-                    .labels(vec!["bug".into(), "feature".into()])
+                    .labels(vec![
+                        LabelVariant::Name("bug".into()),
+                        LabelVariant::WithDescription {
+                            name: "feature".into(),
+                            description: "A new feature".into(),
+                        },
+                    ])
                     .build(),
             )]))
             .build();
